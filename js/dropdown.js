6b12d32b58990528c82033d55e0e68e809fb88a5
EgorZakharenkov <93191584+EgorZakharenkov@users.noreply.github.com>
2023-03-14 14:14:41 +0300

    Add files via upload

--- js/dropdown.js
+++ js/dropdown.js
@@ -15,3 +15,18 @@ window.onclick = function(event) {
         }
     }
 }
+
+
+function inputPrice(){
+    let a = document.querySelector(".value_calck_price");
+    let b = document.querySelector(".slider");
+    a.textContent = b.value * 200000;
+    if(b.value ==0){
+        a.textContent = 50000
+    }
+}
+function inputSrok(){
+    let a = document.querySelector(".value_calk_srok");
+    let b = document.querySelector(".slider_1");
+    a.textContent = b.value;
+}
